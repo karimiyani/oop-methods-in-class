0ee6f085287e05d2e9d0c0cf4714df1b37cfaf7b
karimiyani <front_end2020@yahoo.com>
2021-12-01 23:37:09 +0330

    first commit

--- js/index.js
+++ js/index.js
@@ -4,18 +4,32 @@ class Rectangle {
         this.y = _y ;
         this.color = _color ;
     }
-    getArea() {
+    get area() {
         return this.x * this.y ;
+    }
+    set area (a){
+        this.x = Math.sqrt(a);
+        this.y = this.x  ;
     }
     printDescription() {
        console.log(`this rectangle is ${this.x} *  ${this.y}`)
     }
 
-};
+}
 const rectangle1 = new Rectangle(20, 25, "red");
-console.log(rectangle1.getArea());
+console.log(rectangle1.area);
 //500
 
 const rectangle2 = new Rectangle(30, 35, "red");
-console.log(rectangle2.printDescription());
-// this rectangle is 30 *  35
+console.log(rectangle2.area);
+// 1050
+
+console.log(rectangle1.x);
+//20
+console.log(rectangle2.x);
+//30
+
+
+rectangle2.area = 930;
+console.log(rectangle2.x);
+// 30.495901363953813
